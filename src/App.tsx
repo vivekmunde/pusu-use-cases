df066b0dd2d5a169471d66fce0d798a74e9c423e
Vivek <vivekmunde@gmail.com>
2023-11-29 10:28:20 +0530

    staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import { BrowserRouter } from 'react-router-dom';
+import CodeInline from '../documponents/code-inline';
+import * as LayoutComponents from '../documponents/layout';
+import * as NavigationComponents from '../documponents/navigation';
+import * as PageComponents from '../documponents/page';
+import DocumponentsRoot from '../documponents/root';
 import './App.css';
 
 function App() {
   return (
-    <div className="h-[100vh] w-[100vw] flex justify-center items-center text-primary-600">
-      <h1 className="text-6xl">pusu use cases</h1>
-    </div>
+    <DocumponentsRoot>
+      <BrowserRouter>
+        <LayoutComponents.Layout>
+          <LayoutComponents.Left.Panel>
+            <LayoutComponents.Left.Header>
+              <LayoutComponents.Left.Title>
+                <CodeInline className="bg-primary-600 dark:bg-primary-600 text-white">pusu</CodeInline> use cases
+              </LayoutComponents.Left.Title>
+            </LayoutComponents.Left.Header>
+            <NavigationComponents.Panel>
+              <NavigationComponents.Section>
+                <NavigationComponents.Title>Refresh</NavigationComponents.Title>
+                <NavigationComponents.Link href="#">Read Me</NavigationComponents.Link>
+              </NavigationComponents.Section>
+            </NavigationComponents.Panel>
+          </LayoutComponents.Left.Panel>
+          <LayoutComponents.Content>
+            <PageComponents.Page>
+              <PageComponents.Header>
+                <PageComponents.Title>Use Cases</PageComponents.Title>
+              </PageComponents.Header>
+              <PageComponents.Content>{''}</PageComponents.Content>
+            </PageComponents.Page>
+          </LayoutComponents.Content>
+        </LayoutComponents.Layout>
+      </BrowserRouter>
+    </DocumponentsRoot>
   );
 }
 
